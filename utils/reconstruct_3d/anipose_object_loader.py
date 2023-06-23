c2a68c8d6336684ced556a03b81419c6bc6a10f1
aaroncherian <aaron.cherian2@gmail.com>
2023-06-23 10:40:12 -0400

    adding basic 3d reconstruction

---
+++ utils/reconstruct_3d/anipose_object_loader.py
@@ -0,0 +1,24 @@
+import filecmp
+import logging
+import shutil
+from pathlib import Path
+from typing import Union
+
+from utils.reconstruct_3d import (
+    freemocap_anipose,)
+
+
+
+
+def load_anipose_calibration_toml_from_path(
+        camera_calibration_data_toml_path: Union[str, Path],
+        save_copy_of_calibration_to_this_path: Union[str, Path],
+):
+    try:
+        anipose_calibration_object = freemocap_anipose.CameraGroup.load(str(camera_calibration_data_toml_path))
+
+        return anipose_calibration_object
+    except Exception as e:
+        print(f"Failed to load anipose calibration info from {str(camera_calibration_data_toml_path)}")
+        raise e
+
